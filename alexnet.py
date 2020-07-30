593b38472d4b29e82d31010dae0d058cc0cabd6d
cr.ke <crke@digwise-tech.com>
2020-07-30 02:44:07 -0700

    do alexnet classification and ok, dog!

--- alexnet.py
+++ alexnet.py
@@ -2,11 +2,44 @@ import torch
 import torchvision
 
 #dummy_input = torch.randn(10, 3, 224, 224, device='cpu')
+
+
+
 img_size = (224, 224)
 dummy_input = torch.zeros((10, 3) + img_size)
-model = torchvision.models.alexnet(pretrained=True).cpu()
+model = torchvision.models.vgg16(pretrained=True).cpu()
 
-input_names = ["actual_input_1"] + ["learned_%d" % i for i in range(16) ]
+from torchvision import transforms
+transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485,0.456,0.406],
+        std=[0.299,0.224,0.225]
+        )])
+    
+
+from PIL import Image
+img = Image.open("dog.jpg")
+
+img_t = transform(img)
+batch_t = torch.unsqueeze(img_t, 0)
+
+model.eval()
+out = model(batch_t)
+print(out.shape)
+
+with open('imagenet_classes.txt') as f:
+    classes = [line.strip() for line in f.readlines()]
+
+_, index = torch.max(out, 1)
+percentage = torch.nn.functional.softmax(out, dim=1)[0]*100
+print(classes[index[0]], percentage[index[0]].item())
+print('--end---')
+exit()
+
+input_name = ["actual_input_1"] + ["learned_%d" % i for i in range(16) ]
 output_names = ["output1"]
 
 print(input_names)
